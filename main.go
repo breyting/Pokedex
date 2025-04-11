bbf23b36f3d25a84e7dffd584a8b3b0b9057c7d1
breyting <gathoyematias@gmail.com>
2025-04-11 16:24:43 +0200

    cleaned the main file

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scan := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scan.Scan()
-		command := scan.Text()
-		cleanCommand := cleanInput(command)
-
-		if len(cleanCommand) == 0 {
-			continue
-		}
-		fmt.Printf("Your command was: %s\n", cleanCommand[0])
-	}
+	start_repl()
 }
--- repl.go
+++ repl.go
@@ -1,9 +1,29 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func start_repl() {
+	scan := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scan.Scan()
+		command := scan.Text()
+		cleanCommand := cleanInput(command)
+
+		if len(cleanCommand) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", cleanCommand[0])
+	}
 }
